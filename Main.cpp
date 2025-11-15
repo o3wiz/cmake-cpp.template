a2e7fe9a79374f54745e250fa0fcb64fa69a8856
Daniel Aviv <dani6laviv@gmail.com>
2025-11-16 01:35:25 +0200

    update to work with vs2026

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,15 @@
+#include <algorithm>
+#include <array>
+#include <concepts>
+#include <cstddef>
+#include <functional>
+#include <iterator>
+#include <numeric>
+#include <print>
+#include <ranges>
+#include <print>
+
 int main()
 {
+    std::println("Hello world");
 }
